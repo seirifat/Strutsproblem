f1bf73e467d32d6b830c82eb27017de548aca364
Rifat Firdaus <rifat@Suitmedias-Mac-mini.local>
2015-06-05 11:34:00 +0700

    sei

---
+++ StrutsproblemTests/StrutsproblemTests.m
@@ -0,0 +1,40 @@
+//
+//  StrutsproblemTests.m
+//  StrutsproblemTests
+//
+//  Created by Rifat Firdaus on 6/3/15.
+//  Copyright (c) 2015 Rifat Firdaus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface StrutsproblemTests : XCTestCase
+
+@end
+
+@implementation StrutsproblemTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
